a069613f0a347154f4dd7f8bbae1c4d45434459f
tovare123 <56171397+tovare123@users.noreply.github.com>
2019-10-18 09:38:34 -0700

    Update your_script.R

--- homework/your_script.R
+++ homework/your_script.R
@@ -1,1 +1,50 @@
+######################################
+# 10.02.2019
+# Multiple Linear Regression (MLR) example
+# BISC 481
+######################################
 
+## Install packages
+# Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+# DNAshapeR
+BiocManager::install("DNAshapeR")
+# Caret
+install.packages("caret")
+
+## Initialization
+library(DNAshapeR)
+library(caret)
+workingPath <- "/Users/lizziliz/Downloads/BISC481-master-4/gcPBM/"
+
+## Predict DNA shapes
+fn_fasta <- paste0(workingPath, "Mad.txt.fa")
+pred <- getShape(fn_fasta)
+
+## Encode feature vectors
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
+
+## Build MLR model by using Caret
+# Data preparation
+fn_exp <- paste0(workingPath, "Mad.txt")
+exp_data <- read.table(fn_exp)
+df <- data.frame(affinity=exp_data$V2, featureVector)
+
+# Arguments setting for Caret
+trainControl <- trainControl(method = "cv", number = 10, savePredictions = TRUE)
+
+# Prediction without L2-regularized
+model <- train (affinity~ ., data = df, trControl=trainControl, 
+                method = "lm", preProcess=NULL)
+summary(model)
+
+# Prediction with L2-regularized
+model2 <- train(affinity~., data = df, trControl=trainControl, 
+                method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
+model2
+result <- model2$results$Rsquared[1]
+result
