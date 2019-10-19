23411c5b192dfe9b5d1413f88dc013d89cdb62f9
tovare123 <56171397+tovare123@users.noreply.github.com>
2019-10-18 20:16:46 -0700

    Update your_script.R

--- homework/your_script.R
+++ homework/your_script.R
@@ -1,3 +1,12 @@
+install.packages("BiocManager")
+BiocManager::install()
+BiocManager::install("DNAshapeR")
+
+install.packages("caret")
+
+install.packages("DNAshapeR")
+
+
 ######################################
 # 10.02.2019
 # Multiple Linear Regression (MLR) example
@@ -20,7 +29,7 @@ library(caret)
 workingPath <- "/Users/lizziliz/Downloads/BISC481-master-4/gcPBM/"
 
 ## Predict DNA shapes
-fn_fasta <- paste0(workingPath, "Mad.txt.fa")
+fn_fasta <- paste0(workingPath, "Max.txt.fa")
 pred <- getShape(fn_fasta)
 
 ## Encode feature vectors
@@ -30,7 +39,7 @@ head(featureVector)
 
 ## Build MLR model by using Caret
 # Data preparation
-fn_exp <- paste0(workingPath, "Mad.txt")
+fn_exp <- paste0(workingPath, "Max.txt")
 exp_data <- read.table(fn_exp)
 df <- data.frame(affinity=exp_data$V2, featureVector)
 
@@ -42,9 +51,44 @@ model <- train (affinity~ ., data = df, trControl=trainControl,
                 method = "lm", preProcess=NULL)
 summary(model)
 
-# Prediction with L2-regularized
+# Prediction with L2-regularize
 model2 <- train(affinity~., data = df, trControl=trainControl, 
                 method = "glmnet", tuneGrid = data.frame(alpha = 0, lambda = c(2^c(-15:15))))
 model2
 result <- model2$results$Rsquared[1]
 result
+
+## MER+SEQUENCE
+#max= 0.8646
+#mad=0.8628
+#myc=0.8549
+##MER
+#max=0.7855
+#mad=0.7754
+#myc=0.7785
+#plotting
+--
+  plot.margin = unit(c(0.1, 0.5, 0.1, 0.1), "cm"),
+axis.text.x = element_text(colour="black", size=12),
+axis.text.y = element_text(colour="black", size=12),
+axis.title.x = element_text(colour="black", size=12),
+axis.title.y = element_text(colour="black", size=12),
+panel.grid.major = element_blank(),
+panel.grid.minor = element_blank(),
+panel.background = element_blank(),
+axis.line = element_line(colour = "black"),
+axis.text = element_text(colour ="black"),
+axis.ticks = element_line(colour = "black")
+)
+
+## Data preparation
+SequenceShape <- c(0.8646,0.8628,0.8549)
+Sequence <- c(0.7855,0.7754,0.7785)
+
+
+## Ploting
+ggplot() +
+  geom_point(aes(x = Sequence, y = SequenceShape), color = "red", size=1) +
+  geom_abline(slope=1) + geom_vline(xintercept=0) + geom_hline(yintercept=0) +
+  coord_fixed(ratio = 1, xlim = c(0,1), ylim = c(0,1)) +
+  scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(expand = c(0, 0)) 
